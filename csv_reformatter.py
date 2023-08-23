74ca5549569b0f407a4a159a8670405ccae090fb
Bill Rimell <billjrimell@gmail.com>
2023-08-23 11:04:10 +0100

    changed format to use 1s and 0s instead of "pass" or "fail"

--- csv_reformatter.py
+++ csv_reformatter.py
@@ -1,7 +1,7 @@
 import csv
 
 
-'''
+"""
 
 ----- BEFORE ----
 
@@ -22,16 +22,24 @@ patient_input
 machine_input
 label
 
-'''
+"""
 
-with open('new.csv') as csvfile:
+labels = ["pass", "fail", "Unknown" "topic_marker"]
+
+with open("adversarial_testing.csv") as csvfile:
     with open("converted.csv", "w") as csvConv:
-        reader = csv.reader(csvfile, delimiter=',')
-        writer = csv.writer(csvConv, delimiter=',')
-        
+        reader = csv.reader(csvfile, delimiter=",")
+        writer = csv.writer(csvConv, delimiter=",", lineterminator="\n")
+
         for i, row in enumerate(reader):
+            if row[4] == "Unknown" or row[4] == "topic_marker":
+                continue
+            if row[4] == "pass":
+                row[4] = 1
+            elif row[4] == "fail":
+                row[4] = 0
             converted_row = [row[7], row[2].strip(), row[4]]
             writer.writerow(converted_row)
-        
+
 csvfile.close()
 csvConv.close()
