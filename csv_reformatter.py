cc0ec79b7708f8954e75a9ca3ab5178f15e332f0
Bill Rimell <billjrimell@gmail.com>
2023-08-23 14:42:05 +0100

    calculating the true label in the reformatter (if pass then keep original decision e.g. acceptable -> acceptable, if fail then reverse original decision e.g. acceptable -> unacceptable)

--- csv_reformatter.py
+++ csv_reformatter.py
@@ -24,7 +24,11 @@ true_label
 
 """
 
+output_labels = {"acceptable": 1, "unacceptable": 0}
+rev_output_labels = {"acceptable": 0, "unacceptable": 1}
 labels = ["pass", "fail", "Unknown" "topic_marker"]
+
+duplicate_checker = []
 
 with open("adversarial_testing.csv") as csvfile:
     with open("converted.csv", "w") as csvConv:
@@ -33,17 +37,25 @@ with open("adversarial_testing.csv") as csvfile:
 
         for i, row in enumerate(reader):
             if i == 0:
-                row[7] = "patient_input"
-                row[2] = "machine_input"
-                row[4] = "true_label"
-            if row[4] == "Unknown" or row[4] == "topic_marker":
+                patient_input = "patient_input"
+                machine_input = "machine_input"
+                true_label = "true_label"
+            else:
+                patient_input = row[7]
+                machine_input = row[2].strip()
+
+            if row[4] == "Unknown" or row[4] == "topic_marker" or not row[3] or machine_input in duplicate_checker:
                 continue
+
             if row[4] == "pass":
-                row[4] = 1
+                true_label = output_labels[row[3]]
+
             elif row[4] == "fail":
-                row[4] = 0
-            converted_row = [row[7], row[2].strip(), row[4]]
+                true_label = rev_output_labels[row[3]]
+
+            converted_row = [patient_input, machine_input, true_label]
             writer.writerow(converted_row)
+            duplicate_checker.append(machine_input)
 
 csvfile.close()
 csvConv.close()
